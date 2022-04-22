09448d16b98b8b2187d1f46e142dff2f08ceba36
Hernan <hquintan@ulima.edu.pe>
2022-04-22 16:35:06 -0500

    Pong terminado

--- Assets/Scripts/BallMovementManager.cs
+++ Assets/Scripts/BallMovementManager.cs
@@ -2,23 +2,38 @@
 using UnityEngine;
 using System;
 
+public class GoalScoredData : EventArgs
+{
+    public String jugador;
+}
+
+
 // Observado
 public class BallMovementManager : MonoBehaviour
 {
     public Vector3 initialSpeed;
+    public AudioClip goalSound;
+    public AudioClip paddleCollisionSound;
+    public AudioClip wallCollisionSound;
+
     private Vector3 speed;
 
     private event EventHandler mGoalScored;
+    private bool mIsMoving = true;
+    private AudioSource mAudioSource;
 
     private void Start()
     {
         //StartGame();
+        mAudioSource = GetComponent<AudioSource>();
     }
 
     private void Update()
     {
-        
-        transform.position += (speed * Time.deltaTime);
+        if (mIsMoving)
+        {
+            transform.position += (speed * Time.deltaTime);
+        }
     }
         
     private void OnCollisionEnter2D(Collision2D collision)
@@ -26,6 +41,8 @@ public class BallMovementManager : MonoBehaviour
         
         if (collision.gameObject.CompareTag("Paddle"))
         {
+            mAudioSource.clip = paddleCollisionSound;
+            mAudioSource.Play();
             speed = new Vector3(
                 -speed.x,
                 UnityEngine.Random.Range(-10f, 10f),
@@ -33,6 +50,8 @@ public class BallMovementManager : MonoBehaviour
             );
         }else if (collision.gameObject.CompareTag("Wall"))
         {
+            mAudioSource.clip = wallCollisionSound;
+            mAudioSource.Play();
             speed = new Vector3(
                 speed.x,
                 -speed.y,
@@ -44,13 +63,30 @@ public class BallMovementManager : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Goal!
-        mGoalScored?.Invoke(this, EventArgs.Empty);
+        mAudioSource.clip = goalSound;
+        mAudioSource.Play();
+        mIsMoving = false;
+        GoalScoredData data = new GoalScoredData();
+        if (collision.gameObject.name == "LeftWall")
+        {
+            // Goal del Paddle2
+            data.jugador = "Paddle2";
+            mGoalScored?.Invoke(this, data);
+        }
+        else 
+        {
+            // Goal del Paddle1
+            data.jugador = "Paddle1";
+            mGoalScored?.Invoke(this, data);
+        }
+        
     }
 
     public void StartGame()
     {
         transform.position = new Vector3(0f, 0f, 0f);
         speed = initialSpeed;
+        mIsMoving = true;
     }
 
     public void AddGoalScoredDelegate(EventHandler eventHandler)
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,9 +11,14 @@ public class GameManager : MonoBehaviour
     public GameObject ball;
     public Text tituloUI;
     public Text mensajeUI;
+    public Text score1UI;
+    public Text score2UI;
 
     private bool mRunning = false;
     private BallMovementManager mBallMovementManager;
+
+    private int mScoreJugador1 = 0;
+    private int mScoreJugador2 = 0;
 
     private void Start()
     {
@@ -35,12 +40,34 @@ public class GameManager : MonoBehaviour
         mBallMovementManager.StartGame();
         tituloUI.gameObject.SetActive(false);
         mensajeUI.gameObject.SetActive(false);
+        score1UI.gameObject.SetActive(false);
+        score2UI.gameObject.SetActive(false);
         mRunning = true;
     }
 
     public void OnGoalScoredDelegate(object sender, EventArgs e)
     {
         Debug.Log("Goal");
+
+        // Actualizar los score
+        GoalScoredData data = e as GoalScoredData;
+        if (data.jugador == "Paddle1")
+        {
+            mScoreJugador1++;
+        }else
+        {
+            mScoreJugador2++;
+        }
+
+
+        tituloUI.text = "GOL!";
+        mensajeUI.text = "Presione Espacio para continuar...";
+        score1UI.text = mScoreJugador1.ToString();
+        score2UI.text = mScoreJugador2.ToString();
+        tituloUI.gameObject.SetActive(true);
+        mensajeUI.gameObject.SetActive(true);
+        score1UI.gameObject.SetActive(true);
+        score2UI.gameObject.SetActive(true);
         mRunning = false;
     }
 }
